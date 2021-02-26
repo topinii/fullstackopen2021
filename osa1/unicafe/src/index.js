63736711f52022dce599ea634a84e1f38b0fd994
Topi Niiranen <topi.niiranen@meiko.fi>
2021-02-27 00:45:53 +0200

    uniface step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Statistics = (props) => {
+  console.log('score', props)
+  return (
+  <div>
+    <h1>Statistics</h1>
+    <div>
+      Good: {props.good}
+      <br />
+      Neutral: {props.neutral}
+      <br />
+      Bad: {props.bad}
+      <br />
+      All: {props.bad + props.good + props.neutral}
+      <br />
+      Average: {(props.good + -props.bad) / (props.bad + props.good + props.neutral)}
+      <br />
+      Positive: {(props.good / (props.bad + props.good + props.neutral)) * 100}%
+    </div>
+  </div>
+  )
+}
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
+  const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
@@ -11,35 +32,17 @@ const App = () => {
     <div>
       <h1>Give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>
-          Good
-        </button>
+        <button onClick={() => setGood(good + 1)}>Good</button>
         <br />
         <br />
-        <button onClick={() => setNeutral(neutral + 1)}>
-          Neutral
-        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
         <br />
         <br />
-        <button onClick={() => setBad(bad + 1)}>
-          Bad
-        </button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
-      <div>
-        <h1>Statistics</h1>
-        <div>
-          Good: {good}<br />
-          Neutral: {neutral}<br />
-          Bad: {bad}<br />
-          All: {bad + good + neutral}<br />
-          Average: {((good) + (-bad)) / (bad + good + neutral)}<br />
-          Positive: {(good / (bad + good + neutral)) * 100}%
-        </div>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"))
