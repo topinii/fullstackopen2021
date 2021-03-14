3301b4ad34e023d7e0d1bd25ac4ece1ea15f1498
Topi Niiranen <niiranentopi@gmail.com>
2021-03-14 18:09:44 +0200

    puhelinluettelo step1 fix

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,14 +4,12 @@ import Name from "./components/Name";
 const App = (props) => {
   const [names, setNames] = useState(props.names);
   const [newName, setNewName] = useState("");
-  const [showAll, setShowAll] = useState(true);
 
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
       content: newName,
       id: names.length + 1,
-      important: Math.random() > 0.5,
     };
 
     setNames(names.concat(nameObject));
@@ -19,20 +17,12 @@ const App = (props) => {
   };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
     setNewName(event.target.value);
   };
-
-  const namesToShow = showAll ? names : names.filter((name) => name.important);
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </div>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +33,7 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {namesToShow.map((name) => (
+        {names.map((name) => (
           <Name key={name.id} name={name} />
         ))}
       </ul>
