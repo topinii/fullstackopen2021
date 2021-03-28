3a008072393e51bc4e38143edd87870fd0896d21
Topi Niiranen <niiranentopi@gmail.com>
2021-03-28 13:59:41 +0300

    lomakkeen_kasittely refaktorointia

--- osa2/lomakkeen_kasittely/src/App.js
+++ osa2/lomakkeen_kasittely/src/App.js
@@ -8,18 +8,9 @@ const App = (props) => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  // const hook = () => {
-  //   axios.get("http://localhost:3001/notes").then((response) => {
-  //     console.log("promise fulflilled");
-  //     setNotes(response.data);
-  //   });
-  // };
-
-  // useEffect(hook, []);
-
   useEffect(() => {
-    noteService.getAll().then((response) => {
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -28,12 +19,8 @@ const App = (props) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    // axios.put(url, changedNote).then((response) => {
-    //   setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
-    // });
-
-    noteService.update(id, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
     });
   };
 
@@ -45,13 +32,8 @@ const App = (props) => {
       important: Math.random() > 0.5,
     };
 
-    // axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-    //   setNotes(notes.concat(response.data));
-    //   setNewNote("");
-    // });
-
-    noteService.create(noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
