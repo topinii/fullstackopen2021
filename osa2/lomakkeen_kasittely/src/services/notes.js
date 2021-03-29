067c3a4c29aa7d024dc708c4b04ac69e0f023e00
Topi Niiranen <niiranentopi@gmail.com>
2021-03-29 20:24:04 +0300

    lomakkeen_kasittely oliliterointi

--- osa2/lomakkeen_kasittely/src/services/notes.js
+++ osa2/lomakkeen_kasittely/src/services/notes.js
@@ -3,6 +3,12 @@ const baseUrl = "http://localhost:3001/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  }
   return request.then((response) => response.data);
 };
 
@@ -16,8 +22,4 @@ const update = (id, newObject) => {
   return request.then((response) => response.data);
 };
 
-export default {
-  getAll: getAll,
-  create: create,
-  update: update,
-};
+export default { getAll, create, update };
