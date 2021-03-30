6fc824cce3cd232b0d7ffb1276369b93d8ebd800
Topi Niiranen <niiranentopi@gmail.com>
2021-03-30 15:15:29 +0300

    lomake db nonexisting note

--- osa2/lomakkeen_kasittely/src/services/notes.js
+++ osa2/lomakkeen_kasittely/src/services/notes.js
@@ -9,7 +9,7 @@ const getAll = () => {
     date: '2019-05-30T17:30:31.098Z',
     important: true
   }
-  return request.then((response) => response.data);
+  return request.then((response) => response.data.concat(nonExisting));
 };
 
 const create = (newObject) => {
