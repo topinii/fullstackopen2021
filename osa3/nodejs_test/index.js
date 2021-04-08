caa46984092a5548d5363e50ba1d5eb5b099ee9d
Topi Niiranen <niiranentopi@gmail.com>
2021-04-08 17:17:33 +0300

    json server test

--- osa3/nodejs_test/index.js
+++ osa3/nodejs_test/index.js
@@ -1,8 +1,29 @@
 const http = require("http");
 
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2020-01-10T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2020-01-10T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2020-01-10T19:20:14.298Z",
+    important: true
+  }
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello World");
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(notes));
 });
 
 const port = 3001;
