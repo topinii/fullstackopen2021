5a7e912ae66ae0a5d0b1cb8640c61bdd2ceba039
Topi Niiranen <niiranentopi@gmail.com>
2021-04-19 16:10:55 +0300

    lomake upd

--- osa3/lomakkeen_kasittely/src/App.js
+++ osa3/lomakkeen_kasittely/src/App.js
@@ -29,7 +29,7 @@ const App = (props) => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
     });
-  }, []);
+  }, []);s
 
   const toggleImportanceOf = (id) => {
     const url = `http://localhost:3001/notes/${id}`;
